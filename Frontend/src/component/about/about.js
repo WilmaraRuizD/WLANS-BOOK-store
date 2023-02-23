4826b2a7d702eee898b16159fed04cc505a07619
Natalia <natalia.contactod@gmail.com>
2023-02-23 17:17:47 -0500

    cambios Natalia

---
+++ Frontend/src/component/about/about.js
@@ -0,0 +1,52 @@
+function scrollSection() {
+  let section = document.getElementById("scroll");
+  section.scrollIntoView({behavior: "smooth"});
+  
+}
+
+// tarjetas presentacion 
+
+function mostrarContenido() {
+  let contenido = document.getElementById("card-content1");
+  if (contenido.style.display === "none") {
+    contenido.style.display = "block";
+  } else {
+    contenido.style.display = "none";
+  }
+}
+
+function mostrarContenido2() {
+  let contenido = document.getElementById("card-content2");
+  if (contenido.style.display === "none") {
+    contenido.style.display = "block";
+  } else {
+    contenido.style.display = "none";
+  }
+}
+
+function mostrarContenido3() {
+  let contenido = document.getElementById("card-content3");
+  if (contenido.style.display === "none") {
+    contenido.style.display = "block";
+  } else {
+    contenido.style.display = "none";
+  }
+}
+
+function mostrarContenido4() {
+  let contenido = document.getElementById("card-content4");
+  if (contenido.style.display === "none") {
+    contenido.style.display = "block";
+  } else {
+    contenido.style.display = "none";
+  }
+}
+
+function mostrarContenido5() {
+  let contenido = document.getElementById("card-content5");
+  if (contenido.style.display === "none") {
+    contenido.style.display = "block";
+  } else {
+    contenido.style.display = "none";
+  }
+}
