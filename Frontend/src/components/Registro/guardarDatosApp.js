f98d3cbba4567686d5d1a9b18fb42b6f50fc0735
FelipeAlvarado01 <felipeAF2001alvarado@gmail.com>
2023-03-14 15:20:28 -0500

    redirecciona al menu principal

--- Frontend/src/components/Registro/guardarDatosApp.js
+++ Frontend/src/components/Registro/guardarDatosApp.js
@@ -19,20 +19,19 @@ form.addEventListener('submit', (event) => {
       telefono: telefono,
       password: password
     };
-    /*Guarda un nuevo usuario al localStorage por eso se agregar el  || []*/
+    //Obtengo los usuarios del LocalStorage
     let usuarios = JSON.parse(localStorage.getItem('usuarios')) || []; 
-    usuarios.push(usuario);
+    usuarios.push(usuario); //Y los guardo en este array
     console.log(usuario.length);
-    localStorage.setItem('usuarios', JSON.stringify(usuarios));
-    localStorage.setItem('usuario', JSON.stringify(usuario));
-    console.log('Contraseña confirmada');
-    console.log("Datos guardados");
+
+    //Guardar el nuevo usuario en el LocalStorage
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));//Guarda en la base de usuarios el nuevo usuario
+    localStorage.setItem('usuario', JSON.stringify(usuario));//Guarda un usuario
+
     swal('Felicidades','Su usuario ha sido registrado.','success');
     
   } else {
     swal('Error','Las contraseñas no coinciden.','error');
   }
 
-  //redirigir a otra página
-  //window.location.href = '../Login/LoginIndex.html';
 });
